c42f14693f3674d5261bf5ae3f083d60a76a397e
Evgeniy Bekker <bek17003@byui.edu>
2021-10-13 21:50:43 -0600

    add ability to calculate total hours

--- src/App.js
+++ src/App.js
@@ -6,20 +6,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Welcome to hours tracker!
+        </h1>
+        Input your work hours: <input type="text" id="hourEnteries"></input>
+        <button onClick={calculateHours}>Get total hours</button>
       </header>
     </div>
   );
+}
+
+function calculateHours() {
+  var hourEnteries = document.getElementById("hourEnteries")
+    .value
+    .split(" ")
+    .map(parseTimeEntry)
+    .reduce((minutes1, minutes2) => minutes1 + minutes2);
+  
+  alert(hourEnteries);
+}
+
+function parseTimeEntry(time) {
+  let hours = parseInt(time.split(":")[0]);
+  let minutes = parseInt(time.split(":")[1]);
+
+  return hours * 60 + minutes;
 }
 
 export default App;
